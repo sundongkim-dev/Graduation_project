08434cb47ee40cfcd25067395a91bdeb2cc28d45
김선동 <mok03127@hanyang.ac.kr>
2022-10-28 17:02:12 +0900

    update db_insert and refresh

--- KcBERT/server/db_insert.py
+++ KcBERT/server/db_insert.py
@@ -1,113 +1,63 @@
 '''
-1. mongoDB에 수동으로 저장하는 코드
-2. document 양식 기록
+1. 크롤링한 결과의 mongoDB에 저장하는 코드 -> 추후 서버에서 자동화(insert 및 refresh) 예정
+2. 크롤링 직후의 JSON object 양식
+3. 모델에 돌리고 난 후 refresh된 JSON object 양식
 '''
 from pymongo import MongoClient
-import datetime
-import traceback
+import datetime, traceback
 
-document1 = {
-    "board": "자유게시판",
-    "title": "디논설 수업 시간에 팝콘 쳐먹지 마세요",
-    "content": "개념이 없나?",
-    "username": "익명",
-    "tags": 1,
-    "date": "2022-10-17",
-    "time": "15:15",
-    "comments": [
-        { 
-            "username": "익명1",
-            "message": "팝콘 튀기는 중",
-            "date": "2022-10-17",
-            "time": "15:16",
-            "result": 9,
-            "precision": 88.5
-        },
-        { 
-            "username": "익명2",
-            "message": "ㄹㅇ? 그런 사람이 있다고??",
-            "date": "2022-10-17",
-            "time": "15:17",
-            "result": 9,
-            "precision": 87.6
-        },
-        { 
-            "username": "익명2",
-            "message": "컴공은 벌레들밖엔 없노",
-            "date": "2022-10-17",
-            "time": "16:20",
-            "result": 8,
-            "precision": 54
-        }
-    ]
-}
-
-document2 = {
-    "board": "자유게시판",
-    "title": "대학생 월세 빼고 용돈 얼마가 적절함?",
-    "content": "나 90 받는데\n통신비, 인터넷, 교통비, 관리비만 해도\n30임...",
-    "username": "익명",
-    "tags": 0,
-    "date": "2022-10-17",
-    "time": "17:07",
-    "comments": [
-        { 
-            "username": "익명1",
-            "message": "교통비포함 통신비제외 40",
-            "date": "2022-10-17",
-            "time": "17:09",
-            "result": 9,
-            "precision": 91.5
-        },
-        { 
-            "username": "익명2",
-            "message": "알바를하던가 해라..",
-            "date": "2022-10-17",
-            "time": "17:10",
-            "result": 9,
-            "precision": 72
-        }
-    ]
-}
-
-''' document 포맷
-document_example = {
-    "_id": POST_ID,                         # 게시글 id
-    "board": POST_BOARD,                    # 게시판 이름
-    "title": POST_TITLE,                    # 게시글 제목
-    "content": POST_CONTENT,                # 게시글 내용
-    "username": POST_WRITER,                # 게시글 작성자
-    "tags": POST_RESULT,                    # 혐오 조장 게시글 결과 여부: 0, 1
-    "date": POST_DATE,                      # 게시글 작성 날짜 및 시간
-    "comments": [
-        { 
-            "username": COMMENT_WRITER,     # 댓글 작성자 이름
-            "message": COMMENT_MESSAGE,     # 댓글 내용
-            "time": COMMENT_DATE,           # 댓글 작성 날짜 및 시간
-            "result": COMMENT_RESULT,       # 댓글 분류 결과: 카테고리 명
-            "precision": COMMENT_PRECISION  # 댓글 분류 신뢰도
-        },
-        { 
-            "username": COMMENT_WRITER,
-            "message": COMMENT_MESSAGE,
-            "time": COMMENT_TIME,
-            "result": COMMENT_RESULT,
-            "precision": COMMENT_PRECISION
-        }
-    ]
-}
-'''
-host = 'localhost'
-port = 27017
-if __name__=="__main__":    
+def insertDocuments(host, port, C, objectList):
     try:
         client = MongoClient(host=host, port=port) # 추후 서버 띄우면 아이피 및 포트 변경
         db = client["community_database"]
         collection = db[C]
-        DOCUMENTS = [document1, document2]
-        collection.insert_many(DOCUMENTS)
+        collection.insert_many(objectList)
     except Exception as e:
         print(traceback.format_exc())
     finally:
         client.close()
         print('DB connection closed')
+
+''' 
+크롤링 직후의 JSON object 양식
+document1 = {
+    _id: POST_ID, # '_id'는 db에 insert할 때 자동 생성
+    board: POST_BOARD,
+    title: POST_TITLE,
+    content: POST_CONTENT,
+    username: POST_WRITER,
+    date: POST_DATE,
+    comments: [
+        { 
+            username: COMMENT_WRITER,
+            message: COMMENT_MESSAGE,
+            time: COMMENT_DATE,
+        },
+        { 
+            username: COMMENT_WRITER,
+            mesage: COMMENT_MESSAGE,
+            time: COMMENT_TIME
+        },
+    ]
+}
+
+모델에 돌리고 난 후 refresh된 JSON object 양식
+document1 = {
+    _id: POST_ID, 
+    board: POST_BOARD,
+    title: POST_TITLE,
+    content: POST_CONTENT,
+    username: POST_WRITER,
+    date: POST_DATE,
+    comments: [
+        { 
+            username: COMMENT_WRITER,
+            message: COMMENT_MESSAGE,
+            time: COMMENT_DATE,
+            result: COMMENT_RESULT,
+            precision: RESULT_PRECISION
+        },
+    ]
+    tags: POST_TAGS
+}
+'''
