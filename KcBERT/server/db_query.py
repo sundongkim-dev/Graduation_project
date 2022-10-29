014b09d4da41e33a3e6581c8c96dcf32fe043008
zeunala <zeunala@naver.com>
2022-10-29 18:37:43 +0900

    feat: 로그확인 관련 쿼리 추가 및 호출

--- KcBERT/server/db_query.py
+++ KcBERT/server/db_query.py
@@ -10,7 +10,7 @@ C 커뮤니티의
 1. yy년 mm월의 cleanPost(정상 게시글), hatePost(혐오 게시글)의 수
 2. yy년 mm월의 cleanComment(정상 댓글), curseComment(악플), hateComment(혐오 댓글)의 수
 3. yy년 mm월의 날짜별 hatePost(혐오 게시글), hateComment(혐오+악플)의 수
-4. yy년 mm월 DD일의 cleanPost(정상 게시글), hatePost(혐오조장)의 수
+4. yy년 mm월 DD일의 cleanPost(정상 게시글), hatePost(분쟁글)의 수
 5. yy년 mm월 DD일의 cleanComment(정상 댓글), curseComment(악플), hateComment(혐오 댓글)의 수
 6. yy년 mm월 DD일의 시간별 hatePost(혐오 게시글), hateComment(혐오+악플)의 수
 '''
@@ -47,7 +47,7 @@ def getMonthlyOrDailyPosts(C, yy, mm, dd=0):
         first_condition_result = collection.count_documents({'$and': first_condition})
         res["cleanPost"] = first_condition_result
 
-        # 혐오조장 게시글의 수 - tag == 1
+        # 분쟁 게시글의 수 - tag == 1
         second_condition = [{"date": {'$regex': _date}}, {"tags": 1}]
         second_condition_result = collection.count_documents({'$and': second_condition})
         res["hatePost"] = second_condition_result
@@ -171,7 +171,7 @@ def getDailyOrHourlyHatePostsNComments(C, yy, mm, dd=0):
         
         hatePostlist = [0]*31
         for i in first_condition_result:
-            hatePostlist[int(i['_id'])] = i['count']
+            hatePostlist[int(i['_id']) - 1] = i['count']
         res['hatePost'] = hatePostlist
 
         # 날짜별 혐오 댓글 + 악플 수
@@ -212,8 +212,36 @@ def getDailyOrHourlyHatePostsNComments(C, yy, mm, dd=0):
         hateCommentlist = [0]*31  
         second_condition_result = collection.aggregate(second_condition)
         for i in second_condition_result:
-            hateCommentlist[int(i['_id'])] = i['count']
+            hateCommentlist[int(i['_id']) - 1] = i['count']
         res['hateComment'] = hateCommentlist
+
+        return res
+
+    except Exception as e:
+        print(traceback.format_exc())
+    finally:
+        client.close()
+        print('DB connection closed')
+        
+def getNPosts(C, count=20):
+    '''     
+    Args:
+        C: 커뮤니티의 이름(문자열), DB 접속 위함
+        count: 가져올 글 개수
+    
+    Returns:
+        C 커뮤니티의 글 20개를 뒤에서 조회해서 배열로 반환
+    '''
+    # DB 연결
+    try:
+        client = MongoClient(host=host, port=port) # 추후 서버 띄우면 아이피 및 포트 변경
+        db = client["community_database"]
+        collection = db[C]
+
+        res = []
+        resultList = collection.find({}, {"_id": 0}).sort("date", -1).limit(count)
+        for result in resultList:
+            res.append(result)
 
         return res
 
@@ -227,5 +255,6 @@ if __name__=="__main__":
     #res = getMonthlyOrDailyPosts("everytime", 22, 10, 18)
     #res = getMonthlyOrDailyComments("everytime", 22, 10, 17)
     #res = getDailyHatePostsNComments("everytime", 22, 10)
-    res = getDailyOrHourlyHatePostsNComments("everytime", 22, 10)
+    #res = getDailyOrHourlyHatePostsNComments("everytime", 22, 10)
+    res = getNPosts("everytime", count=20)
     print(res)
